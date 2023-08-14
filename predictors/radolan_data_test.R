484dae034ea6ae0b38b0898df03fb9b035cb5500
MaikenBaumberger <maiken.baumberger@uni-muenster.de>
2023-08-14 15:35:51 +0200

    radolan test

---
+++ predictors/radolan_data_test.R
@@ -0,0 +1,65 @@
+####
+#This script downloads the recent RADOLAN RW data from
+#https://opendata.dwd.de/climate_environment/CDC/grids_germany/hourly/radolan/recent/
+# Historical data can be downloaded from https://opendata.dwd.de/climate_environment/CDC/grids_germany/hourly/radolan/historical/asc/
+# In this case change url below
+
+
+library(terra)
+##################
+# PARAMETERS (NEED USER ADJUSTMENT)
+##################
+
+### date to download
+date <- "202206"
+# destination of downloaded files
+testfolder <- "C:/Users/maike/Desktop/Carbon4D/GitHub_soil_temperature_moisture_4d_data/radolan/test2/"
+
+##################
+# Download and extract
+##################
+
+url <- paste0("https://opendata.dwd.de/climate_environment/CDC/grids_germany/hourly/radolan/historical/asc/2022/RW-",date,".tar")
+#url <- paste0("https://opendata.dwd.de/climate_environment/CDC/grids_germany/hourly/radolan/historical/bin/2022/RW",date,".tar.gz")
+url
+testfile <- paste0(testfolder,"RW",date,".tar")
+testfile
+testfolder
+download.file(url, testfile,  mode="wb")
+untar(testfile,exdir=testfolder)
+untar("C:/Users/maike/Desktop/Carbon4D/GitHub_soil_temperature_moisture_4d_data/radolan/test2/RW-20220601.tar.gz",exdir=testfolder)
+
+
+##################
+# Select all rasters from the selected date
+##################
+#files <- list.files(testfolder,pattern=".asc$", full.names=TRUE)
+files <- list.files(testfolder,pattern="asc$", full.names=TRUE)
+files <- files[grepl(date,files)]
+
+##################
+# Read raster and project
+##################
+
+
+
+file <- terra::rast(files)
+crs(file) <- "+proj=stere +lat_0=90.0 +lon_0=10.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m"
+
+##################
+# Visualize
+##################
+
+plot(file[[4]])
+
+file
+
+### optional:
+library(mapview)
+library(raster)
+file_rast = raster(file[[1]])
+mapview(file_rast)
+
+file_rast
+
+###################
